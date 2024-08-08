e7d2956d289a379a368dbef295450a3e7bb4ce36
Doeun <id5eun30@gmail.com>
2024-08-08 09:00:34 +0900

    fix: 채용공고 수정 기능 에러 수정
    - 채용공고 수정 POSTMAN test중 에러 발견
    - @PathVariable에 파라미터 이름 명시하여 해결

--- src/main/java/com/group/recruitment/controller/company/CompanyController.java
+++ src/main/java/com/group/recruitment/controller/company/CompanyController.java
@@ -22,8 +22,8 @@ public class CompanyController {
     }
 
     // 채용공고 수정
-    @PutMapping("/jobs/{id}")
-    public void updateJobPosting(@PathVariable Long id, @RequestBody JobPostingDTO jobPostingDTO) {
+    @PutMapping("/jobs/{id}")   // 에러해결: 1) @PathVariable 속성 이름 기재, 2) -parameters 옵션
+    public void updateJobPosting(@PathVariable("id") Long id, @RequestBody JobPostingDTO jobPostingDTO) {
         jobPostingService.updateJobPosting(id, jobPostingDTO);
     }
 
